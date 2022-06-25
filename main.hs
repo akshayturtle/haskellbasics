e2ce2bf0bc42d105cb2656917e79d6ca04322137
Akshay Gupta <72372808+turtletechsaiprojects@users.noreply.github.com>
2022-06-25 19:35:00 +0000

    currying example

--- main.hs
+++ main.hs
@@ -2,7 +2,9 @@ isIn c (d: rest) = if c == d then True else isIn c rest
 
 isIn _ [] = False
 
-main = do
-print $ isIn 'm' "mamoth"
-print $ isIn 3 [3 ,5, 6]
-print $ isIn 'a' in ['a', 'b']
+isInCurry :: [Char] -> Bool
+
+isInCurry = isIn 'a'
+
+
+main = print $ isInCurry "abc"
