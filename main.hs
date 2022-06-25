5eb6705f02f410629be944b64a96c9ab5751570d
Akshay Gupta <72372808+turtletechsaiprojects@users.noreply.github.com>
2022-06-25 14:50:45 +0000

    Custom Data Types in Haskell

--- main.hs
+++ main.hs
@@ -1,3 +1,23 @@
-addNumbers x = x + x
+-- tokenize :: String -> String
 
-main = print $ addNumbers $ addNumbers 25
+-- tokenize is a function that takes in String as input and output will be String
+
+-- tokenize :: String -> [String]
+
+-- tokenize is a function that takes in String as input and output will be List of String
+
+data Animal = Dog | Cat 
+
+-- Animal is a custom data type that can have 2 values Dog or Cat. Here Dog and Cat are constructors. Constructors should start with a caps letter
+
+-- Here Dog and Cat are types
+
+-- In haskell data types are immutable
+
+getAnimalSound :: Animal -> String 
+
+getAnimalSound Dog = "woof"
+
+getAnimalSound Cat = "meow"
+
+main = print $ getAnimalSound Cat
