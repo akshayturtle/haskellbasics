f044d7c6142bd8dac2c078947401f62d25eebb97
Akshay Gupta <72372808+turtletechsaiprojects@users.noreply.github.com>
2022-06-25 14:22:02 +0000

    Syntax Change

--- main.hs
+++ main.hs
@@ -1,8 +1,8 @@
-setDate day month year = month ++ " " ++ day ++ ", " ++ year
+setDate (day, month, year) = month ++ " " ++ day ++ ", " ++ year
 
 
 day = "1st"
 month = "July"
 year = "2000"
 
-main = putStrLn $ setDate day month year
+main = putStrLn $ setDate (day, month, year)
