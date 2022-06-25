8f9e499d0a51d97431a4462128df9f6ce127ff71
Akshay Gupta <72372808+turtletechsaiprojects@users.noreply.github.com>
2022-06-25 14:56:01 +0000

    building a loop with recursion

--- main.hs
+++ main.hs
@@ -1,23 +1,9 @@
--- tokenize :: String -> String
+main :: IO()
+-- IO() tells that we can take input from user and perform output
 
--- tokenize is a function that takes in String as input and output will be String
-
--- tokenize :: String -> [String]
-
--- tokenize is a function that takes in String as input and output will be List of String
-
-data Animal = Dog | Cat 
-
--- Animal is a custom data type that can have 2 values Dog or Cat. Here Dog and Cat are constructors. Constructors should start with a caps letter
-
--- Here Dog and Cat are types
-
--- In haskell data types are immutable
-
-getAnimalSound :: Animal -> String 
-
-getAnimalSound Dog = "woof"
-
-getAnimalSound Cat = "meow"
-
-main = print $ getAnimalSound Cat
+main = do
+userInput <- getLine
+-- getLine takes input from user and store in userInput
+print userInput
+main
+-- adding main at the end makes the call recursive 
